91dca01e7cdd8c69dd8398dfda046f70dbfadb42
omiambhorkar <omiambhorkar@gmail.com>
2018-05-12 14:55:18 +0530

    ok

--- src/main/java/com/onkyarity/controller/TestController.java
+++ src/main/java/com/onkyarity/controller/TestController.java
@@ -1,10 +1,10 @@
 package com.onkyarity.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class TestController {
 	
 	@RequestMapping("/add")
